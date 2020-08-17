0ad91145b1a8bbc1e7746ef1ebc1ce939aaee238
alpha <alphadev@localhost.localdomain>
2020-08-17 11:13:47 -0300

    Atividade 01

---
+++ estr_date/aula01/ex01.cpp
@@ -0,0 +1,19 @@
+
+#include "iostream"
+#include "math.h"
+using namespace std;
+
+int main ()
+{
+    int x [] = {1, 0, -1};
+
+    int n = sizeof(x)/sizeof(int);
+
+    int aux = 0;
+
+    for (int i =0; i<n; i++)
+    {
+        aux = pow(x[i], 2) + x[i];
+        cout << aux << endl;
+    }
+}
---
+++ estr_date/aula01/tarefa.cpp
@@ -0,0 +1,57 @@
+
+/**
+ * Exercício: Idade para votar 
+ * Aluno: Jose Neto Guimarães Andrade
+ * Orientador: Elizeu Lemes
+ * Estrutura de dados
+ *
+ **/
+
+#include "iostream"
+#include "math.h"
+using namespace std;
+
+int     main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    string nome [] = {"Sara Lemes", "Ricardo Jafé"};  
+    double  salario  []  =  {12000,  10243.20};  
+    int  tecla, idade[]={30,  45};
+
+    menu:
+    system("clear");
+    cout << "==========================================\n";
+    cout << "Escolha uma opção:\n1- Exibir\n2- Exibir com aumento\n3 - Sair\n";
+    cout << "==========================================\n";
+    cin >> tecla;
+    system("clear");
+
+    switch(tecla)
+    {
+        case 1:
+        for (int i = 0; i < 2; i++)
+        {
+            cout << "Nome: " << nome[i] << "\nSalário: " << salario[i] << "\nIdade: " << idade[i] << "\n\n"; 
+        }
+        cout << "Aperte qualquer tecla para voltar ao menu!";
+        getchar();
+        getchar();
+        goto menu;
+
+        case 2:
+        for (int i = 0; i < 2; i++)
+        {
+           cout << "Nome: " << nome[i] << "\nSalário: " << salario[i] * 1.1 << "\nIdade: " << idade[i] << "\n\n";
+        }
+        cout << "Aperte qualquer tecla para voltar ao menu!";
+        getchar();
+        getchar();
+        goto menu;
+
+        case 3:
+        break;
+    }
+    
+    return (0);
+}
